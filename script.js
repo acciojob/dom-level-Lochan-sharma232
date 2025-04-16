c317fe533499e720b172583db2a776401f2cf3e2
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-16 13:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-//your JS code here. If required.
-let mybody=document.getElementById("level");
-let count=1;
-count(n>=5)
-mybody.innerHTML+=`
-<li id="level">
-</li>
-`;
-mybody.appendChild(level);
-alert(`The level of the element is:{n}`)
+var mymsg=document.getElementById("level");
+var level=0;
+while(msg.parentNode){
+	level++;
+
+msg=msg.parentNode;
+}
+var display ="The level of the element is"+level;
+alert(display);
