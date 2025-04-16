d4f119aba64d9dde7424edaea578b3b978b11358
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-16 12:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 //your JS code here. If required.
 let mybody=document.getElementById("level");
+let count=1;
+count(n>=5)
 mybody.innerHTML+=`
 <li id="level">
 </li>
 `;
 mybody.appendChild(level);
-alert("The level of the element is:{n}");
+alert(`The level of the element is:{n}`)
